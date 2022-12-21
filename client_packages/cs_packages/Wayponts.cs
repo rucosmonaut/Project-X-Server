1e8320792762758785269579f34df9a36ea86f37
rucosmonaut <fraitoggg@mail.ru>
2022-12-21 22:55:21 +0300

    init

---
+++ client_packages/cs_packages/Wayponts.cs
@@ -0,0 +1,18 @@
+﻿using RAGE;
+using System;
+
+namespace RolePlayClient
+{
+    public class Wayponts : RAGE.Events
+    {
+        public Wayponts()
+        {
+            RAGE.Events.OnPlayerCreateWaypoint += OnWaypointCreated;
+        }
+
+        public void OnWaypointCreated(Vector3 position)
+        {
+            RAGE.Chat.Output($"You have just created a waypoint with coords: {position}");
+        }
+    }
+}
